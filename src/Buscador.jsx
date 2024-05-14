b74a25698c7e95965a66e7bafce5a08e142e140e
pauval <pauly.val@gmail.com>
2024-05-13 23:26:17 -0700

    Initial commit

---
+++ src/Buscador.jsx
@@ -0,0 +1,53 @@
+
+import './Buscador.css'; // Archivo de estilos CSS para Buscador
+import React, { useState } from 'react';
+
+function Buscador({ feriados }) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = () => {
+    const filteredResults = feriados.filter(feriado => {
+      return (
+        feriado.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        feriado.date.includes(searchTerm) ||
+        feriado.type.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        (feriado.extra && feriado.extra.toLowerCase().includes(searchTerm.toLowerCase()))
+      );
+    });
+
+    // Ordenar los resultados por fecha
+    const sortedResults = filteredResults.sort((a, b) => {
+      return new Date(a.date) - new Date(b.date);
+    });
+
+    setSearchResults(sortedResults);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Buscar..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Buscar</button>
+      <h2>Resultados de la búsqueda:</h2>
+      <ul>
+        {searchResults.map((feriado, index) => (
+          <li key={index}>
+            <strong>Título:</strong> {feriado.title}<br />
+            <strong>Fecha:</strong> {feriado.date}<br />
+            <strong>Tipo:</strong> {feriado.type}<br />
+            <strong>Inalienable:</strong> {feriado.inalienable ? 'Sí' : 'No'}<br />
+            {feriado.extra && <strong>Extra:</strong>} {feriado.extra}
+            <hr />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Buscador;
---
+++ src/MiApi.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import './MiApi.css'; // Archivo de estilos CSS para Buscador
+
+function MiApi() {
+  const [feriados, setFeriados] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.boostr.cl/feriados/en.json');
+        const data = await response.json();
+        console.log('Data from API:', data); // Verificar los datos recibidos de la API en la consola
+        setFeriados(data.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <p>Cargando...</p>;
+  }
+
+  if (!Array.isArray(feriados)) {
+    console.error('Error: los datos de feriados no son un array:', feriados);
+    return null;
+  }
+
+  return (
+    <div>
+      <h2>Feriados</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Fecha</th>
+            <th>Tipo</th>
+            <th>Inalienable</th>
+            <th>Extra</th>
+          </tr>
+        </thead>
+        <tbody>
+          {feriados.map((feriado, index) => (
+            <tr key={index}>
+              <td>{feriado.title}</td>
+              <td>{feriado.date}</td>
+              <td>{feriado.type}</td>
+              <td>{feriado.inalienable ? 'Sí' : 'No'}</td>
+              <td>{feriado.extra}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default MiApi;
+
